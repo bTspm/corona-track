0e5e3bd66c969bcda2ba2a3c6f8e752bdeba6753
bTspm <btspm10@gmail.com>
2020-04-16 20:58:11 -0400

    - Update version.

--- app/models/concerns/api/ninja_client.rb
+++ app/models/concerns/api/ninja_client.rb
@@ -2,29 +2,33 @@ module Api
   class NinjaClient < Client
 
     def latest_global_stats
-      fetch_cached("#{self.class.name}/#{__method__}") { get("#{_base_url}/all") }
+      fetch_cached("#{self.class.name}/#{__method__}") { get("#{_base_url}/#{_version}/all") }
     end
 
     def global_time_series
-      fetch_cached("#{self.class.name}/#{__method__}") { get("#{_base_url}/v2/historical?lastdays=all") }
+      fetch_cached("#{self.class.name}/#{__method__}") { get("#{_base_url}/#{_version}/historical?lastdays=all") }
     end
 
     def latest_countries_stats
-      fetch_cached("#{self.class.name}/#{__method__}") { get("#{_base_url}/countries?sort=cases") }
+      fetch_cached("#{self.class.name}/#{__method__}") { get("#{_base_url}/#{_version}/countries?sort=cases") }
     end
 
     def latest_country_stats_by_country_code(code)
-      fetch_cached("#{self.class.name}/#{__method__}/#{code}") { get("#{_base_url}/countries/#{code}?strict=false") }
+      fetch_cached("#{self.class.name}/#{__method__}/#{code}") { get("#{_base_url}/#{_version}/countries/#{code}?strict=false") }
     end
 
     def country_time_series_by_country_code(code)
-      fetch_cached("#{self.class.name}/#{__method__}/#{code}") { get("#{_base_url}/v2/historical/#{code}?lastdays=all") }
+      fetch_cached("#{self.class.name}/#{__method__}/#{code}") { get("#{_base_url}/#{_version}/historical/#{code}?lastdays=all") }
     end
 
     private
 
     def _base_url
       "https://corona.lmao.ninja"
+    end
+
+    def _version
+      ENV["NINJA_VERSION"] || "v2"
     end
   end
 end
